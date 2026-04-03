f185f28710f6b20021ba1b69a0396bdc6292e4d0
Payan3521 <pablobedoya3521@gmail.com>
2026-04-03 15:47:08 -0500

    INFO: Infra

--- emergency-microservice/src/main/java/com/backend/emergency_microservice/controllers/TestController.java
+++ emergency-microservice/src/main/java/com/backend/emergency_microservice/controllers/TestController.java
@@ -1,4 +1,4 @@
-package com.backend.emergency_microservice;
+package com.backend.emergency_microservice.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
